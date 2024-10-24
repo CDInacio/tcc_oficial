6b25c9be2db9f5467d4d9204b5a58fc398722686
claudiodantas <claudiodantas1996@gmail.com>
2024-10-24 18:30:34 -0300

    Add SigninForm component to Signin page

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -1,8 +1,14 @@
+import SigninForm from "@/components/signin/SigninForm";
+
 const Signin = () => {
   return (
     <div className="w-screen h-screen flex">
-      <div className="w-[45%] bg-teal-700">d</div>
-      <div className="w-[55%] h-full bg-red-400">d</div>
+      <div className="w-[50%] bg-background flex items-center justify-center">
+        <div className="w-[500px]">
+          <SigninForm />
+        </div>
+      </div>
+      <div className="w-[50%] h-full bg-red-400">d</div>
     </div>
   );
 };
