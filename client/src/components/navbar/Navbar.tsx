67d4c42dd89675b9cedb778cc6ff9e843b0ed57c
claudiodantas <claudiodantas1996@gmail.com>
2024-11-07 18:57:54 -0300

    Add Navbar component

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+// Navbar.jsx
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-16 text-gray-800 bg-white flex items-center p-4 z-10">
+      <h1 className="text-lg font-bold">My Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
