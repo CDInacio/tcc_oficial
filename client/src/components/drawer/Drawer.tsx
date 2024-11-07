483d787c9dab8a6238fe5b61d24bb822c529e0a7
claudiodantas <claudiodantas1996@gmail.com>
2024-11-07 18:57:42 -0300

    Refactor Drawer component to add new routes and user information

--- client/src/components/drawer/Drawer.tsx
+++ client/src/components/drawer/Drawer.tsx
@@ -1,7 +1,15 @@
-import { IoHomeOutline } from "react-icons/io5";
+import {
+  IoBodyOutline,
+  IoFolderOutline,
+  IoHomeOutline,
+  IoPersonOutline,
+} from "react-icons/io5";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "../ui/separator";
+import useAuthStore from "@/store/user.store";
+import { firstTwoNames } from "@/utils/textFormate";
+import { useNavigate } from "react-router-dom";
 
 interface IDrawerItem {
   name: string;
@@ -15,9 +23,31 @@ const drawerItems: IDrawerItem[] = [
     icon: <IoHomeOutline className="w-6 h-6 lg:w-4 lg:h-4" />,
     path: "/",
   },
+  {
+    name: "Pacientes",
+    icon: <IoBodyOutline className="w-6 h-6 lg:w-4 lg:h-4" />,
+    path: "/pacientes",
+  },
+  {
+    name: "Agendamentos",
+    icon: <IoFolderOutline className="w-6 h-6 lg:w-4 lg:h-4" />,
+    path: "/agendamentos",
+  },
+  {
+    name: "Usuários",
+    icon: <IoPersonOutline className="w-6 h-6 lg:w-4 lg:h-4" />,
+    path: "/usuarios",
+  },
 ];
 
 const Drawer = () => {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <div
       className={`lg:w-60 w-20 bg-white border-l border-[1px]  h-screen transition-all duration-300 fixed z-50 overflow-hidden`}
@@ -30,7 +60,7 @@ const Drawer = () => {
           </Avatar>
 
           <h2 className="hidden lg:flex justify-center text-xl font-medium text-[#333333] mt-5">
-            Fulano de Tal
+            {firstTwoNames(user?.nomeCompleto!)}
           </h2>
           <p className="hidden lg:flex  justify-center text-[#9E9E9E]">
             Administrador
@@ -39,8 +69,9 @@ const Drawer = () => {
           <div>
             {drawerItems.map((item: IDrawerItem, index: number) => (
               <div
+                onClick={() => handleNavigate(item.path)}
                 key={index}
-                className="flex items-center space-x-2 p-2 cursor-pointer hover:bg-[#014C90] transition duration-300 hover:text-white text-[#9E9E9E] rounded-md"
+                className="my-2 flex items-center space-x-2 p-2 cursor-pointer hover:bg-[#014C90] transition duration-300 hover:text-white text-[#9E9E9E] rounded-md"
               >
                 <span className="mx-auto lg:mx-0">{item.icon}</span>
                 <span className="hidden lg:flex">{item.name}</span>
